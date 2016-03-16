534ad096e6aec4f88bc3cfe6e39f7c9dd9edd63f
xieguigang <xie.guigang@gmail.com>
2016-03-16 19:33:39 +0800

    indicator works fine!

--- Microsoft.VisualBasic.Windows.Forms/Form1.Designer.vb
+++ Microsoft.VisualBasic.Windows.Forms/Form1.Designer.vb
@@ -22,33 +22,20 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New Microsoft.VisualBasic.Windows.Forms.Gtk.CSSEngine.Components.Button()
-        Me.DividerLabel1 = New Microsoft.VisualBasic.Windows.Forms.Gtk.CSSEngine.Components.DividerLabel()
-        Me.Pane1 = New Microsoft.VisualBasic.Windows.Forms.Gtk.CSSEngine.Components.Pane()
         Me.TabControl1 = New Microsoft.VisualBasic.Windows.Forms.TabControl()
+        Me.Pane1 = New Microsoft.VisualBasic.Windows.Forms.Gtk.CSSEngine.Components.Pane()
+        Me.DividerLabel1 = New Microsoft.VisualBasic.Windows.Forms.Gtk.CSSEngine.Components.DividerLabel()
+        Me.Button1 = New Microsoft.VisualBasic.Windows.Forms.Gtk.CSSEngine.Components.Button()
+        Me.TabIndicator1 = New Microsoft.VisualBasic.Windows.Forms.TabIndicator()
         Me.SuspendLayout()
         '
-        'Button1
+        'TabControl1
         '
-        Me.Button1.BackColor = System.Drawing.Color.DimGray
-        Me.Button1.ImageOver = Nothing
-        Me.Button1.ImageUp = Nothing
-        Me.Button1.Location = New System.Drawing.Point(300, 33)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        '
-        'DividerLabel1
-        '
-        Me.DividerLabel1.AutoSize = True
-        Me.DividerLabel1.LineStyle = System.Windows.Forms.Border3DStyle.Etched
-        Me.DividerLabel1.Location = New System.Drawing.Point(480, 33)
-        Me.DividerLabel1.Name = "DividerLabel1"
-        Me.DividerLabel1.Size = New System.Drawing.Size(72, 13)
-        Me.DividerLabel1.Spacing = 0
-        Me.DividerLabel1.TabIndex = 1
-        Me.DividerLabel1.Text = "DividerLabel1"
+        Me.TabControl1.Location = New System.Drawing.Point(41, 169)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.Size = New System.Drawing.Size(638, 339)
+        Me.TabControl1.TabIndex = 3
+        Me.TabControl1.TabLabelSkin = Nothing
         '
         'Pane1
         '
@@ -62,18 +49,44 @@ Partial Class Form1
         Me.Pane1.Size = New System.Drawing.Size(200, 100)
         Me.Pane1.TabIndex = 2
         '
-        'TabControl1
+        'DividerLabel1
         '
-        Me.TabControl1.Location = New System.Drawing.Point(41, 169)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.Size = New System.Drawing.Size(638, 339)
-        Me.TabControl1.TabIndex = 3
+        Me.DividerLabel1.AutoSize = True
+        Me.DividerLabel1.LineStyle = System.Windows.Forms.Border3DStyle.Etched
+        Me.DividerLabel1.Location = New System.Drawing.Point(480, 33)
+        Me.DividerLabel1.Name = "DividerLabel1"
+        Me.DividerLabel1.Size = New System.Drawing.Size(72, 13)
+        Me.DividerLabel1.Spacing = 0
+        Me.DividerLabel1.TabIndex = 1
+        Me.DividerLabel1.Text = "DividerLabel1"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DimGray
+        Me.Button1.ImageOver = Nothing
+        Me.Button1.ImageUp = Nothing
+        Me.Button1.Location = New System.Drawing.Point(300, 33)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        '
+        'TabIndicator1
+        '
+        Me.TabIndicator1.DefaultDock = 0
+        Me.TabIndicator1.Location = New System.Drawing.Point(41, 139)
+        Me.TabIndicator1.Name = "TabIndicator1"
+        Me.TabIndicator1.PointerDock = 0
+        Me.TabIndicator1.Size = New System.Drawing.Size(553, 15)
+        Me.TabIndicator1.Speed = 3
+        Me.TabIndicator1.TabIndex = 4
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(730, 560)
+        Me.Controls.Add(Me.TabIndicator1)
         Me.Controls.Add(Me.TabControl1)
         Me.Controls.Add(Me.Pane1)
         Me.Controls.Add(Me.DividerLabel1)
@@ -89,4 +102,5 @@ Partial Class Form1
     Friend WithEvents DividerLabel1 As Gtk.CSSEngine.Components.DividerLabel
     Friend WithEvents Pane1 As Gtk.CSSEngine.Components.Pane
     Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabIndicator1 As TabIndicator
 End Class
---
+++ Microsoft.VisualBasic.Windows.Forms/TabControl/TabIndicator.vb
@@ -0,0 +1,47 @@
+﻿Imports Microsoft.VisualBasic.Parallel
+
+Public Class TabIndicator : Implements IDisposable
+
+    'ReadOnly __animatedTimer As New Timer With {
+    '    .Interval = 2,
+    '    .Enabled = True
+    '}
+
+    Public Property Speed As Integer = 30
+
+    Public Property DefaultDock As Integer
+    Public Property PointerDock As Integer
+
+    Private Sub TabIndicator_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' AddHandler __animatedTimer.Tick, AddressOf __runDock
+        Call RunTask(AddressOf __animatedWorker)
+    End Sub
+
+    Private Sub __runDock(sender As Object, e As EventArgs)
+        If Math.Abs(Panel1.Location.X - PointerDock) <= Speed Then
+            Panel1.Location = New Point(PointerDock, Panel1.Location.Y)
+        ElseIf Panel1.Location.X < PointerDock Then
+            Panel1.Location = New Point(Panel1.Location.X + Speed, Panel1.Location.Y)
+        ElseIf Panel1.Location.X = PointerDock Then
+        Else
+            Panel1.Location = New Point(Panel1.Location.X - Speed, Panel1.Location.Y)
+        End If
+    End Sub
+
+    Private Sub __animatedWorker()
+        Do While True
+            Try
+                Call Me.Invoke(Sub() __runDock(Nothing, Nothing))
+            Catch ex As Exception
+                If Me.IsDisposed Then
+                    Exit Do
+                End If
+            End Try
+            Call Threading.Thread.Sleep(2)
+        Loop
+    End Sub
+
+    'Private Sub TabIndicator_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+    '    PointerDock = e.X
+    'End Sub
+End Class
