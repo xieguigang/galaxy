3715aa95191676ab649e19b022efda548572875f
xieguigang <xie.guigang@gmail.com>
2016-04-04 00:16:09 +0800

    add source code original copyrights

--- Microsoft.VisualBasic.Windows.Forms/Form/API/Dwm/DwmAPI.vb
+++ Microsoft.VisualBasic.Windows.Forms/Form/API/Dwm/DwmAPI.vb
@@ -2,6 +2,16 @@
 
 Namespace API.Dwm
 
+    ''' <summary>
+    ''' Metro UI (Zune like) Interface (form)
+    ''' lipinho, 27 Dec 2010 CPOL
+    '''
+    ''' The future of Windows Interfaces is probably the Zune-like ones, with a borderless form and some controls inside of it.
+    ''' The problem is: if you're using WindowsForm, creating that borderless form with shadows and resizing stuff isn't as easy as it seems.
+    ''' This article will show you how to create those forms using a bit of DWM and some other Windows APIs.
+    '''
+    ''' (http://www.codeproject.com/Articles/138661/Metro-UI-Zune-like-Interface-form)
+    ''' </summary>
     Public Module DwmAPI
 
         ' Fields
--- Microsoft.VisualBasic.Windows.Forms/Form/Form.vb
+++ Microsoft.VisualBasic.Windows.Forms/Form/Form.vb
@@ -1,4 +1,14 @@
 ﻿
+''' <summary>
+''' Metro UI (Zune like) Interface (form)
+''' lipinho, 27 Dec 2010 CPOL
+'''
+''' The future of Windows Interfaces is probably the Zune-like ones, with a borderless form and some controls inside of it.
+''' The problem is: if you're using WindowsForm, creating that borderless form with shadows and resizing stuff isn't as easy as it seems.
+''' This article will show you how to create those forms using a bit of DWM and some other Windows APIs.
+'''
+''' (http://www.codeproject.com/Articles/138661/Metro-UI-Zune-like-Interface-form)
+''' </summary>
 Public Class Form
 
 End Class
