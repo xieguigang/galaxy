adcf84373caf579079fc3da940c6b62a1407d4e3
guigang xie <xie.guigang@gcmodeller.org>
2023-01-20 11:23:43 +0800

    there is a bug about text pattern test

--- DataValidation/TextValidates.vb
+++ DataValidation/TextValidates.vb
@@ -6,13 +6,20 @@ Public Module TextValidates
     Const title As String = "Text Validation"
 
     <Extension>
-    Public Function ValidateDouble(txt As TextBox, Optional fieldName As String = Nothing) As Double?
+    Public Function ValidateDouble(txt As TextBox,
+                                   Optional fieldName As String = Nothing,
+                                   Optional pip As Object = "") As Double?
+
+        If pip Is Nothing Then
+            Return Nothing
+        End If
+
         Dim prompt As String = If(fieldName.StringEmpty, "", $"({fieldName}: double)")
 
         If txt.Text.StringEmpty Then
             MessageBox.Show($"A required{prompt} text input should not be empty!", title, MessageBoxButtons.OK, MessageBoxIcon.Information)
             Return Nothing
-        ElseIf txt.Text.IsNumeric Then
+        ElseIf txt.Text.IsNumeric(includesInteger:=True) Then
             MessageBox.Show($"A required{prompt} text input should be a number!", title, MessageBoxButtons.OK, MessageBoxIcon.Information)
             Return Nothing
         Else
@@ -21,7 +28,14 @@ Public Module TextValidates
     End Function
 
     <Extension>
-    Public Function ValidateInteger(txt As TextBox, Optional fieldName As String = Nothing) As Integer?
+    Public Function ValidateInteger(txt As TextBox,
+                                    Optional fieldName As String = Nothing,
+                                    Optional pip As Object = "") As Integer?
+
+        If pip Is Nothing Then
+            Return Nothing
+        End If
+
         Dim prompt As String = If(fieldName.StringEmpty, "", $"({fieldName}: integer)")
 
         If txt.Text.StringEmpty Then
