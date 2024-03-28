7fcd48dba1d7698023295da7297cc821f5ba32d2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-28 13:44:21 +0800

    fix of the missing code commits

---
+++ DataValidation/UIInteractive.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Windows.Forms
+
+Public Module UIInteractive
+
+    <Extension>
+    Public Sub SetAutoSelectRow(dv As DataGridView)
+        AddHandler dv.CellClick,
+            Sub(sender, evt)
+                If evt.RowIndex >= 0 AndAlso evt.RowIndex < dv.Rows.Count Then
+                    dv.Rows(evt.RowIndex).Selected = True
+                End If
+            End Sub
+    End Sub
+End Module
