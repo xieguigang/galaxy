40f3ef318f4998022562f7ace3a3e05864db73ae
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-22 09:37:15 +0800

    make improvements of the render code

--- JsonViewer/JsonViewer.Designer.vb
+++ JsonViewer/JsonViewer.Designer.vb
@@ -26,127 +26,97 @@ Partial Class JsonViewer
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Me.components = New System.ComponentModel.Container()
-            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(JsonViewer))
-            Me.tvJson = New System.Windows.Forms.TreeView()
-            Me.mnuTree = New System.Windows.Forms.ContextMenuStrip(Me.components)
-            Me.mnuFind = New System.Windows.Forms.ToolStripMenuItem()
-            Me.mnuExpandAll = New System.Windows.Forms.ToolStripMenuItem()
-            Me.toolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
-            Me.mnuCopy = New System.Windows.Forms.ToolStripMenuItem()
-            Me.mnuCopyValue = New System.Windows.Forms.ToolStripMenuItem()
-            Me.imgList = New System.Windows.Forms.ImageList(Me.components)
-            Me.mnuVisualizerPnl = New System.Windows.Forms.ContextMenuStrip(Me.components)
-            Me.mnuShowOnRight = New System.Windows.Forms.ToolStripMenuItem()
-            Me.mnuShowOnBottom = New System.Windows.Forms.ToolStripMenuItem()
-            Me.mnuTree.SuspendLayout()
-            Me.mnuVisualizerPnl.SuspendLayout()
-            Me.SuspendLayout()
-            '
-            'tvJson
-            '
-            Me.tvJson.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-            Me.tvJson.ContextMenuStrip = Me.mnuTree
-            Me.tvJson.Dock = System.Windows.Forms.DockStyle.Fill
-            Me.tvJson.HideSelection = False
-            Me.tvJson.ImageIndex = 0
-            Me.tvJson.ImageList = Me.imgList
-            Me.tvJson.Location = New System.Drawing.Point(0, 0)
-            Me.tvJson.Name = "tvJson"
-            Me.tvJson.SelectedImageIndex = 0
-            Me.tvJson.Size = New System.Drawing.Size(792, 558)
-            Me.tvJson.TabIndex = 3
-            '
-            'mnuTree
-            '
-            Me.mnuTree.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFind, Me.mnuExpandAll, Me.toolStripMenuItem1, Me.mnuCopy, Me.mnuCopyValue})
-            Me.mnuTree.Name = "mnuTree"
-            Me.mnuTree.Size = New System.Drawing.Size(134, 98)
-            '
-            'mnuFind
-            '
-            Me.mnuFind.Name = "mnuFind"
-            Me.mnuFind.Size = New System.Drawing.Size(133, 22)
-            Me.mnuFind.Text = "&Find"
-            '
-            'mnuExpandAll
-            '
-            Me.mnuExpandAll.Name = "mnuExpandAll"
-            Me.mnuExpandAll.Size = New System.Drawing.Size(133, 22)
-            Me.mnuExpandAll.Text = "Expand &All"
-            '
-            'toolStripMenuItem1
-            '
-            Me.toolStripMenuItem1.Name = "toolStripMenuItem1"
-            Me.toolStripMenuItem1.Size = New System.Drawing.Size(130, 6)
-            '
-            'mnuCopy
-            '
-            Me.mnuCopy.Name = "mnuCopy"
-            Me.mnuCopy.Size = New System.Drawing.Size(133, 22)
-            Me.mnuCopy.Text = "&Copy"
-            '
-            'mnuCopyValue
-            '
-            Me.mnuCopyValue.Name = "mnuCopyValue"
-            Me.mnuCopyValue.Size = New System.Drawing.Size(133, 22)
-            Me.mnuCopyValue.Text = "Copy &Value"
-            '
-            'imgList
-            '
-            Me.imgList.ImageStream = CType(resources.GetObject("imgList.ImageStream"), System.Windows.Forms.ImageListStreamer)
-            Me.imgList.TransparentColor = System.Drawing.Color.White
-            Me.imgList.Images.SetKeyName(0, "obj.bmp")
-            Me.imgList.Images.SetKeyName(1, "array.bmp")
-            Me.imgList.Images.SetKeyName(2, "prop.bmp")
-            '
-            'mnuVisualizerPnl
-            '
-            Me.mnuVisualizerPnl.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuShowOnRight, Me.mnuShowOnBottom})
-            Me.mnuVisualizerPnl.Name = "mnuVisualizerPnl"
-            Me.mnuVisualizerPnl.Size = New System.Drawing.Size(115, 48)
-            '
-            'mnuShowOnRight
-            '
-            Me.mnuShowOnRight.Checked = True
-            Me.mnuShowOnRight.CheckOnClick = True
-            Me.mnuShowOnRight.CheckState = System.Windows.Forms.CheckState.Checked
-            Me.mnuShowOnRight.Name = "mnuShowOnRight"
-            Me.mnuShowOnRight.Size = New System.Drawing.Size(114, 22)
-            Me.mnuShowOnRight.Text = "&Right"
-            '
-            'mnuShowOnBottom
-            '
-            Me.mnuShowOnBottom.CheckOnClick = True
-            Me.mnuShowOnBottom.Name = "mnuShowOnBottom"
-            Me.mnuShowOnBottom.Size = New System.Drawing.Size(114, 22)
-            Me.mnuShowOnBottom.Text = "&Bottom"
-            '
-            'JsonViewer
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
-            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-            Me.Controls.Add(Me.tvJson)
-            Me.Name = "JsonViewer"
-            Me.Size = New System.Drawing.Size(792, 558)
-            Me.mnuTree.ResumeLayout(False)
-            Me.mnuVisualizerPnl.ResumeLayout(False)
-            Me.ResumeLayout(False)
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(JsonViewer))
+        Me.tvJson = New System.Windows.Forms.TreeView()
+        Me.mnuTree = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.mnuFind = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExpandAll = New System.Windows.Forms.ToolStripMenuItem()
+        Me.toolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.mnuCopy = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuCopyValue = New System.Windows.Forms.ToolStripMenuItem()
+        Me.imgList = New System.Windows.Forms.ImageList(Me.components)
+        Me.mnuTree.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tvJson
+        '
+        Me.tvJson.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.tvJson.ContextMenuStrip = Me.mnuTree
+        Me.tvJson.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tvJson.HideSelection = False
+        Me.tvJson.ImageIndex = 0
+        Me.tvJson.ImageList = Me.imgList
+        Me.tvJson.Location = New System.Drawing.Point(0, 0)
+        Me.tvJson.Name = "tvJson"
+        Me.tvJson.SelectedImageIndex = 0
+        Me.tvJson.Size = New System.Drawing.Size(792, 558)
+        Me.tvJson.TabIndex = 3
+        '
+        'mnuTree
+        '
+        Me.mnuTree.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFind, Me.mnuExpandAll, Me.toolStripMenuItem1, Me.mnuCopy, Me.mnuCopyValue})
+        Me.mnuTree.Name = "mnuTree"
+        Me.mnuTree.Size = New System.Drawing.Size(181, 120)
+        '
+        'mnuFind
+        '
+        Me.mnuFind.Name = "mnuFind"
+        Me.mnuFind.Size = New System.Drawing.Size(180, 22)
+        Me.mnuFind.Text = "&Find"
+        '
+        'mnuExpandAll
+        '
+        Me.mnuExpandAll.Name = "mnuExpandAll"
+        Me.mnuExpandAll.Size = New System.Drawing.Size(180, 22)
+        Me.mnuExpandAll.Text = "Expand &All"
+        '
+        'toolStripMenuItem1
+        '
+        Me.toolStripMenuItem1.Name = "toolStripMenuItem1"
+        Me.toolStripMenuItem1.Size = New System.Drawing.Size(177, 6)
+        '
+        'mnuCopy
+        '
+        Me.mnuCopy.Name = "mnuCopy"
+        Me.mnuCopy.Size = New System.Drawing.Size(180, 22)
+        Me.mnuCopy.Text = "&Copy"
+        '
+        'mnuCopyValue
+        '
+        Me.mnuCopyValue.Name = "mnuCopyValue"
+        Me.mnuCopyValue.Size = New System.Drawing.Size(180, 22)
+        Me.mnuCopyValue.Text = "Copy &Value"
+        '
+        'imgList
+        '
+        Me.imgList.ImageStream = CType(resources.GetObject("imgList.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.imgList.TransparentColor = System.Drawing.Color.White
+        Me.imgList.Images.SetKeyName(0, "obj.bmp")
+        Me.imgList.Images.SetKeyName(1, "array.bmp")
+        Me.imgList.Images.SetKeyName(2, "prop.bmp")
+        '
+        'JsonViewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.tvJson)
+        Me.Name = "JsonViewer"
+        Me.Size = New System.Drawing.Size(792, 558)
+        Me.mnuTree.ResumeLayout(False)
+        Me.ResumeLayout(False)
 
-        End Sub
+    End Sub
 
 #End Region
-        Private imgList As ImageList
+    Private imgList As ImageList
         Private WithEvents tvJson As TreeView
         Private mnuTree As ContextMenuStrip
-        Private mnuFind As ToolStripMenuItem
-        Private mnuExpandAll As ToolStripMenuItem
-        Private mnuVisualizerPnl As ContextMenuStrip
-        Private mnuShowOnRight As ToolStripMenuItem
-        Private mnuShowOnBottom As ToolStripMenuItem
-        Private toolStripMenuItem1 As ToolStripSeparator
-        Private mnuCopy As ToolStripMenuItem
-        Private mnuCopyValue As ToolStripMenuItem
+    Private WithEvents mnuFind As ToolStripMenuItem
+    Private WithEvents mnuExpandAll As ToolStripMenuItem
+    Private toolStripMenuItem1 As ToolStripSeparator
+    Private WithEvents mnuCopy As ToolStripMenuItem
+    Private WithEvents mnuCopyValue As ToolStripMenuItem
 
-    End Class
+End Class
 
--- JsonViewer/JsonViewer.vb
+++ JsonViewer/JsonViewer.vb
@@ -42,11 +42,37 @@ Partial Public Class JsonViewer : Inherits UserControl
         End Set
     End Property
 
+    Public Event FindAction(node As JsonViewerTreeNode)
+
     Sub New()
         Call InitializeComponent()
 
         viewer = New JsonRender(tvJson)
         viewer.Render("{}")
+    End Sub
+
+    Private Sub mnuExpandAll_Click(sender As Object, e As System.EventArgs) Handles mnuExpandAll.Click
+        If Not viewer Is Nothing Then
+            Call viewer.ExpandAll()
+        End If
+    End Sub
+
+    Private Sub mnuFind_Click(sender As Object, e As System.EventArgs) Handles mnuFind.Click
+        If Not viewer Is Nothing Then
+            RaiseEvent FindAction(viewer.GetSelectedTreeNode)
+        End If
+    End Sub
+
+    Private Sub mnuCopy_Click(sender As Object, e As System.EventArgs) Handles mnuCopy.Click
+        If Not viewer Is Nothing Then
+
+        End If
+    End Sub
+
+    Private Sub mnuCopyValue_Click(sender As Object, e As System.EventArgs) Handles mnuCopyValue.Click
+        If Not viewer Is Nothing Then
+
+        End If
     End Sub
 End Class
 
