1cd0ddf9f739d3cc080be38f19c537d76a1d82a2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-28 11:35:05 +0800

    add resource resx xml file model

--- src/ResourcesEditor/Form1.vb
+++ src/ResourcesEditor/Form1.vb
@@ -1,9 +1,11 @@
 ﻿Public Class Form1
 
+    Dim vbproj As String
+
     Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
         Using file As New OpenFileDialog With {.Filter = "Visual Basci Project(*.vbproj)|*.vbproj"}
             If file.ShowDialog = DialogResult.OK Then
-
+                vbproj = file.FileName
             End If
         End Using
     End Sub
