99d216264650ba183cffc2bf962955e132afa4f6
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-28 10:37:35 +0800

    add dialog ui

---
+++ src/GalaxyWorkbench/AppHost.vb
@@ -0,0 +1,8 @@
+﻿Public Interface AppHost
+
+    Event ResizeForm(location As Point, size As Size)
+
+    Function GetDesktopLocation() As Point
+    Function GetClientSize() As Size
+
+End Interface
---
+++ src/GalaxyWorkbench/CommonRuntime.vb
@@ -0,0 +1,9 @@
+﻿Public Module CommonRuntime
+
+    Public ReadOnly Property AppHost As AppHost
+
+    Public Sub Hook(apphost As AppHost)
+        _AppHost = apphost
+    End Sub
+
+End Module
