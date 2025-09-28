a8911391e924bde5a4490cc2db93fe86355ec725
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-28 10:47:05 +0800

    resource file error

--- src/GalaxyData/test/Form1.vb
+++ src/GalaxyData/test/Form1.vb
@@ -1,4 +1,10 @@
-﻿Public Class Form1
+﻿Imports Galaxy.Data.JSON
+Imports Galaxy.Workbench
+Imports Galaxy.Workbench.CommonDialogs
+
+Public Class Form1 : Implements AppHost
+
+    Public Event ResizeForm As AppHost.ResizeFormEventHandler Implements AppHost.ResizeForm
 
     Private Sub JsonViewer1_Load(sender As Object, e As EventArgs) Handles JsonViewer1.Load
         JsonViewer1.RootTag = "Hello World"
@@ -15,4 +21,24 @@
 'd': 123456
 }"
     End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        RaiseEvent ResizeForm(Location, Size)
+    End Sub
+
+    Private Sub JsonViewer1_FindAction(node As JsonViewerTreeNode) Handles JsonViewer1.FindAction
+        Call InputDialog.Input(Of Form2)()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Call CommonRuntime.Hook(Me)
+    End Sub
+
+    Public Function GetDesktopLocation() As Point Implements AppHost.GetDesktopLocation
+        Return Location
+    End Function
+
+    Public Function GetClientSize() As Size Implements AppHost.GetClientSize
+        Return Size
+    End Function
 End Class
