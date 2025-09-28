c0ec78f158ce272b3b1ae6624ce0fabb61eb5ed2
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-28 11:41:12 +0800

    encode image?

--- src/ResourcesEditor/FormResxEditor.vb
+++ src/ResourcesEditor/FormResxEditor.vb
@@ -6,7 +6,12 @@
         Using file As New OpenFileDialog With {.Filter = "Visual Basci Project(*.vbproj)|*.vbproj"}
             If file.ShowDialog = DialogResult.OK Then
                 vbproj = file.FileName
+                loadResourceFile()
             End If
         End Using
+    End Sub
+
+    Private Sub loadResourceFile()
+
     End Sub
 End Class
