7e5d7672c11274cf4879b48b8413cd644913eb28
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-09 11:59:03 +0800

    apply reference to the new ui project

--- src/GalaxyWorkbench/AppHost.vb
+++ src/GalaxyWorkbench/AppHost.vb
@@ -1,6 +1,11 @@
-﻿Public Interface AppHost
+﻿
+''' <summary>
+''' the main framework of the workbench desktop application and the plugin system base
+''' </summary>
+Public Interface AppHost
 
-    Property WindowState As FormWindowState
+    ReadOnly Property ClientRectangle As Rectangle
+
     ''' <summary>
     ''' get/set current actived mdi document
     ''' </summary>
@@ -8,12 +13,21 @@
     ReadOnly Property ActiveDocument As Form
 
     Event ResizeForm(location As Point, size As Size)
+    Event CloseWorkbench(args As FormClosingEventArgs)
+
+    Sub SetWorkbenchVisible(visible As Boolean)
+    Sub SetWindowState(stat As FormWindowState)
 
     Function GetDesktopLocation() As Point
     Function GetClientSize() As Size
     Function GetDocuments() As IEnumerable(Of Form)
     Function GetDockPanel() As Control
+    Function GetWindowState() As FormWindowState
 
+    Sub SetTitle(title As String)
     Sub StatusMessage(msg As String, Optional icon As Image = Nothing)
+    Sub Warning(msg As String)
+    Sub LogText(text As String)
+    Sub ShowProperties(obj As Object)
 
 End Interface
