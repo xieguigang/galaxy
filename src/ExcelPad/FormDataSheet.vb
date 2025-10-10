a145ef8018ab48e2c9b60a28b8ecef4c5a0c7e0d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-10 19:11:10 +0800

    load table data

--- src/ExcelPad/FormDataSheet.vb
+++ src/ExcelPad/FormDataSheet.vb
@@ -1,4 +1,7 @@
-﻿Imports Galaxy.Data.TableSheet
+﻿Imports System.Data
+Imports System.Runtime.CompilerServices
+Imports System.Windows.Forms
+Imports Galaxy.Data.TableSheet
 Imports Microsoft.VisualBasic.Data.Framework.IO
 
 Public Class FormDataSheet
@@ -6,15 +9,40 @@ Public Class FormDataSheet
     Dim WithEvents table As New AdvancedDataGridView
     Dim WithEvents toolbar As New AdvancedDataGridViewSearchToolBar
 
+    Dim loader As GridLoaderHandler
+    Dim search As GridSearchHandler
+    Dim source As New BindingSource
+
     Private Sub FormDataSheet_Load(sender As Object, e As EventArgs) Handles Me.Load
         Controls.Add(table)
         Controls.Add(toolbar)
 
         table.Dock = System.Windows.Forms.DockStyle.Fill
         toolbar.Dock = System.Windows.Forms.DockStyle.Top
+        loader = New GridLoaderHandler(table, toolbar, source)
+        search = New GridSearchHandler(table)
     End Sub
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Overloads Sub LoadData(table As DataFrameResolver)
+        Call loader.LoadTable(Sub(tbl) LoadData(tbl, table))
+    End Sub
 
+    Private Overloads Sub LoadData(tbl As DataTable, table As DataFrameResolver)
+        Dim nsize As Integer = table.HeadTitles.Length
+
+        For Each col As String In table.HeadTitles
+            Call tbl.Columns.Add(col, GetType(String))
+        Next
+
+        For Each row As RowObject In table.Rows
+            Dim data As Object() = New Object(nsize - 1) {}
+
+            For i As Integer = 0 To data.Length - 1
+                data(i) = row(i)
+            Next
+
+            Call tbl.Rows.Add(data)
+        Next
     End Sub
 End Class
