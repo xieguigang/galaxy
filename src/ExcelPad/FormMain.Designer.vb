00fe4de48a9530df7f016179cc1021c637040813
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-10 18:05:07 +0800

    fix of the missing string resources

--- src/ExcelPad/FormMain.Designer.vb
+++ src/ExcelPad/FormMain.Designer.vb
@@ -24,17 +24,26 @@ Partial Class FormMain
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.VisualStudioToolStripExtender1 = New Microsoft.VisualStudio.WinForms.Docking.VisualStudioToolStripExtender(Me.components)
+        Me.DockPanel1 = New Microsoft.VisualStudio.WinForms.Docking.DockPanel()
         Me.SuspendLayout()
         '
         'VisualStudioToolStripExtender1
         '
         Me.VisualStudioToolStripExtender1.DefaultRenderer = Nothing
         '
+        'DockPanel1
+        '
+        Me.DockPanel1.Location = New System.Drawing.Point(155, 144)
+        Me.DockPanel1.Name = "DockPanel1"
+        Me.DockPanel1.Size = New System.Drawing.Size(200, 100)
+        Me.DockPanel1.TabIndex = 0
+        '
         'FormMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DockPanel1)
         Me.Name = "FormMain"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -42,4 +51,5 @@ Partial Class FormMain
     End Sub
 
     Friend WithEvents VisualStudioToolStripExtender1 As Microsoft.VisualStudio.WinForms.Docking.VisualStudioToolStripExtender
+    Friend WithEvents DockPanel1 As Microsoft.VisualStudio.WinForms.Docking.DockPanel
 End Class
