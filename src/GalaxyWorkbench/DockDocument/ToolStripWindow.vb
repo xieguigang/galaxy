06f30b6a47e475eebe38817fa23bef2c5f31bbe7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-11 10:57:20 +0800

    move document window source files into base workbench framework project

---
+++ src/GalaxyWorkbench/DockDocument/ToolStripWindow.vb
@@ -0,0 +1,11 @@
+﻿''' <summary>
+''' A method of <see cref="GetMainToolStrip"/> must be overrides
+''' and implemented for get the toolstrip for the external 3rd 
+''' plugin
+''' </summary>
+Public Class ToolStripWindow
+
+    Public Overridable Function GetMainToolStrip() As ToolStrip
+        Throw New NotImplementedException
+    End Function
+End Class
