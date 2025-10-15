575582885bd6150b23f15d85208e8a8d7602f7e7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-15 16:27:48 +0800

    config ui settings?

---
+++ src/GalaxyWorkbench/Container/Settings/UISettings.vb
@@ -0,0 +1,13 @@
+﻿Namespace Container
+
+    Public Class UISettings
+
+        Public Property language As Languages = Languages.Default
+        Public Property width As Integer
+        Public Property height As Integer
+        Public Property left As Integer
+        Public Property top As Integer
+        Public Property rememberLocation As Boolean = True
+
+    End Class
+End Namespace
