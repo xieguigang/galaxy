478ba656bc5600546620d53d4ac6c7eb8984652e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-10-15 16:15:34 +0800

    check  of the development environment

---
+++ src/GalaxyWorkbench/Container/AppEnvironment.vb
@@ -0,0 +1,19 @@
+﻿Namespace Container
+
+    ''' <summary>
+    ''' A shared model for test application environment
+    ''' </summary>
+    Public Module AppEnvironment
+
+        Public ReadOnly Property IsDevelopmentMode As Boolean
+
+        Sub New()
+            IsDevelopmentMode = CheckDevelopmentMode()
+        End Sub
+
+        Private Function CheckDevelopmentMode() As Boolean
+            Return True
+        End Function
+
+    End Module
+End Namespace
