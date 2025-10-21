af1c2a966ffa3c2a4283434229721bc477f1f05d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-10-21 10:37:59 +0800

    make improements of the ui binding

---
+++ src/RibbonEventBinding/BindingAction.vb
@@ -0,0 +1,14 @@
+﻿Public Interface IBindingAction
+    Sub ClearHook()
+End Interface
+
+Public Class BindingAction
+
+    Public Sub ClearRibbonHook(ParamArray bindings As IBindingAction())
+        For Each button As IBindingAction In bindings
+            If Not button Is Nothing Then
+                Call button.ClearHook()
+            End If
+        Next
+    End Sub
+End Class
