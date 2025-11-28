1f2d9a8c6a5ee3e4e0b97c7bca0677320a14d0f8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-28 14:05:18 +0800

    ExpandPropertyInGrid

---
+++ src/GalaxyData/DefaultExpandedArray.vb
@@ -0,0 +1,84 @@
+﻿Imports System.ComponentModel
+Imports System.Reflection
+
+Public Class DefaultExpandedArray : Inherits ExpandableObjectConverter
+
+    Public Overrides Function GetProperties(
+        context As ITypeDescriptorContext,
+        value As Object,
+        attributes() As Attribute) As PropertyDescriptorCollection
+
+        ' 获取默认的属性描述符集合
+        Dim pds As PropertyDescriptorCollection = MyBase.GetProperties(context, value, attributes)
+        If value IsNot Nothing AndAlso TypeOf value Is String() Then
+            Dim stringArray As String() = DirectCast(value, String())
+            Dim propList As New List(Of PropertyDescriptor)
+
+            ' 为数组中的每个元素创建一个属性描述符
+            For i As Integer = 0 To stringArray.Length - 1
+                Dim index As Integer = i ' 闭包问题，需要局部变量
+                Dim pd As New ArrayPropertyDescriptor(stringArray, index)
+                propList.Add(pd)
+            Next
+
+            Return New PropertyDescriptorCollection(propList.ToArray())
+        Else
+            Return pds
+        End If
+    End Function
+
+    Public Overrides Function GetPropertiesSupported(
+        context As ITypeDescriptorContext) As Boolean
+        Return True
+    End Function
+End Class
+
+Public Class ArrayPropertyDescriptor : Inherits PropertyDescriptor
+
+    Private ReadOnly _array As String()
+    Private ReadOnly _index As Integer
+
+    Public Sub New(array As String(), index As Integer)
+        MyBase.New($"#{index + 1}", Nothing)
+        _array = array
+        _index = index
+    End Sub
+
+    Public Overrides ReadOnly Property ComponentType As Type
+        Get
+            Return GetType(String())
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property IsReadOnly As Boolean
+        Get
+            Return False
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property PropertyType As Type
+        Get
+            Return GetType(String)
+        End Get
+    End Property
+
+    Public Overrides Function CanResetValue(component As Object) As Boolean
+        Return False
+    End Function
+
+    Public Overrides Function GetValue(component As Object) As Object
+        Return _array(_index)
+    End Function
+
+    Public Overrides Sub ResetValue(component As Object)
+    End Sub
+
+    Public Overrides Sub SetValue(component As Object, value As Object)
+        _array(_index) = If(value?.ToString(), "")
+        OnValueChanged(component, EventArgs.Empty)
+    End Sub
+
+    Public Overrides Function ShouldSerializeValue(component As Object) As Boolean
+        Return False
+    End Function
+End Class
--- src/GalaxyWorkbench/DockDocument/Presets/PropertyWindow.vb
+++ src/GalaxyWorkbench/DockDocument/Presets/PropertyWindow.vb
@@ -58,6 +58,7 @@
 #End Region
 
 Imports System.ComponentModel
+Imports System.Reflection
 Imports System.Runtime.CompilerServices
 
 Namespace DockDocument.Presets
@@ -77,9 +78,18 @@ Namespace DockDocument.Presets
         ''' set selected object and then refresh the <see cref="propertyGrid"/>
         ''' </summary>
         ''' <param name="obj"></param>
-        Public Sub SetObject(obj As Object)
+        ''' <param name="expands">
+        ''' a list of the property name that should be in expaned status by default.
+        ''' </param>
+        Public Sub SetObject(obj As Object, ParamArray expands As String())
             propertyGrid.SelectedObject = obj
             propertyGrid.Refresh()
+
+            If Not expands Is Nothing Then
+                For Each name As String In expands
+                    Call ExpandPropertyInGrid(propertyGrid, name)
+                Next
+            End If
         End Sub
 
         Private Sub PropertyWindow_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
@@ -90,6 +100,57 @@ Namespace DockDocument.Presets
         Private Sub PropertyWindow_Load(sender As Object, e As EventArgs) Handles Me.Load
             DoubleBuffered = True
             TabText = CommonStrings.GetString("property_window")
+        End Sub
+
+        ''' <summary>
+        ''' 使用反射展开 PropertyGrid 中的指定属性。
+        ''' </summary>
+        ''' <param name="grid">目标 PropertyGrid 控件。</param>
+        ''' <param name="propertyName">要展开的属性的名称（不区分大小写）。</param>
+        Public Shared Sub ExpandPropertyInGrid(grid As PropertyGrid, propertyName As String)
+            If grid Is Nothing OrElse String.IsNullOrWhiteSpace(propertyName) Then Return
+            If grid.SelectedObject Is Nothing Then Return
+
+            Try
+                Call ExpandPropertyInGridInternal(grid, propertyName)
+            Catch ex As Exception
+                ' 反射操作可能会因为.NET版本更新而失败，这里进行捕获以避免程序崩溃
+                System.Diagnostics.Debug.WriteLine($"展开属性时出错: {ex.Message}")
+            End Try
+        End Sub
+
+        Private Shared Sub ExpandPropertyInGridInternal(grid As PropertyGrid, propertyName As String)
+            ' 1. 获取 PropertyGrid 的内部 PropertyGridView 对象
+            ' 这个内部对象是真正负责绘制和管理的类
+            Dim gridViewField = GetType(PropertyGrid).GetField("gridView", BindingFlags.NonPublic Or BindingFlags.Instance)
+            If gridViewField Is Nothing Then Return ' 兼容性检查
+            Dim gridView = gridViewField.GetValue(grid)
+
+            ' 2. 获取 PropertyGridView 的所有行
+            Dim rowsProperty = gridView.GetType().GetProperty("Rows", BindingFlags.NonPublic Or BindingFlags.Instance)
+            If rowsProperty Is Nothing Then Return
+            Dim gridEntries = rowsProperty.GetValue(gridView)
+
+            ' 3. 遍历所有行，找到目标属性
+            If TypeOf gridEntries Is IEnumerable Then
+                For Each entry As Object In CType(gridEntries, IEnumerable)
+                    ' 检查 entry 是否有效，并获取其标签
+                    If entry IsNot Nothing Then
+                        Dim labelProperty = entry.GetType().GetProperty("Label", BindingFlags.Public Or BindingFlags.Instance)
+                        If labelProperty IsNot Nothing Then
+                            Dim label = CStr(labelProperty.GetValue(entry))
+
+                            ' 找到匹配的属性名（不区分大小写）
+                            If String.Equals(label, propertyName, StringComparison.OrdinalIgnoreCase) Then
+                                ' 4. 找到后，调用其内部的 Expand() 方法
+                                Dim expandMethod = entry.GetType().GetMethod("Expand", BindingFlags.Public Or BindingFlags.Instance)
+                                expandMethod?.Invoke(entry, Nothing)
+                                Exit For ' 找到后就退出循环
+                            End If
+                        End If
+                    End If
+                Next
+            End If
         End Sub
     End Class
 End Namespace
