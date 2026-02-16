a5555131ae4141671763a6e12ad8c3a32a31b911
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-02-16 16:47:51 +0800

    downgrade nuget package version from preview release to stable release

--- src/GalaxyData/JSONViewer/Models/JsonTreeObjectTypeDescriptor.vb
+++ src/GalaxyData/JSONViewer/Models/JsonTreeObjectTypeDescriptor.vb
@@ -1,71 +1,87 @@
 ﻿Imports System.ComponentModel
+Imports System.Runtime.CompilerServices
 
 Namespace JSON.Models
 
     Friend Class JsonTreeObjectTypeDescriptor
         Implements ICustomTypeDescriptor
-        Private _jsonObject As JsonObject
-        Private _propertyCollection As PropertyDescriptorCollection
+
+        ReadOnly _jsonObject As JsonObject
+        ReadOnly _propertyCollection As PropertyDescriptorCollection
 
         Public Sub New(jsonObject As JsonObject)
             _jsonObject = jsonObject
-            InitPropertyCollection()
+            _propertyCollection = New PropertyDescriptorCollection(InitPropertyCollection(jsonObject).ToArray)
         End Sub
 
-        Private Sub InitPropertyCollection()
-            Dim propertyDescriptors As List(Of PropertyDescriptor) = New List(Of PropertyDescriptor)()
+        Public ReadOnly Property RequireRegisteredTypes As Boolean? Implements ICustomTypeDescriptor.RequireRegisteredTypes
 
-            For Each field In _jsonObject.Fields
-                Dim pd As PropertyDescriptor = New JsonTreeObjectPropertyDescriptor(field)
-                propertyDescriptors.Add(pd)
+        Private Shared Iterator Function InitPropertyCollection(jsonObject As JsonObject) As IEnumerable(Of PropertyDescriptor)
+            For Each field As JsonObject In jsonObject.Fields
+                Yield New JsonTreeObjectPropertyDescriptor(field)
             Next
-            _propertyCollection = New PropertyDescriptorCollection(propertyDescriptors.ToArray())
-        End Sub
+        End Function
 
+        Public Function GetConverterFromRegisteredType() As TypeConverter Implements ICustomTypeDescriptor.GetConverterFromRegisteredType
+            Return Nothing
+        End Function
+
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function GetAttributes() As AttributeCollection Implements ICustomTypeDescriptor.GetAttributes
             Return TypeDescriptor.GetAttributes(_jsonObject, True)
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function GetClassName() As String Implements ICustomTypeDescriptor.GetClassName
             Return TypeDescriptor.GetClassName(_jsonObject, True)
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function GetComponentName() As String Implements ICustomTypeDescriptor.GetComponentName
             Return TypeDescriptor.GetComponentName(_jsonObject, True)
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function GetConverter() As TypeConverter Implements ICustomTypeDescriptor.GetConverter
             Return TypeDescriptor.GetConverter(_jsonObject, True)
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function GetDefaultEvent() As EventDescriptor Implements ICustomTypeDescriptor.GetDefaultEvent
             Return TypeDescriptor.GetDefaultEvent(_jsonObject, True)
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function GetDefaultProperty() As PropertyDescriptor Implements ICustomTypeDescriptor.GetDefaultProperty
             Return Nothing
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function GetEditor(editorBaseType As Type) As Object Implements ICustomTypeDescriptor.GetEditor
             Return TypeDescriptor.GetEditor(_jsonObject, editorBaseType, True)
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function GetEvents1(attributes As Attribute()) As EventDescriptorCollection Implements ICustomTypeDescriptor.GetEvents
             Return TypeDescriptor.GetEvents(Me, attributes, True)
         End Function
 
-        Private Function GetEvents2() As EventDescriptorCollection Implements ICustomTypeDescriptor.GetEvents
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Private Function GetEvents2() As EventDescriptorCollection Implements ICustomTypeDescriptor.GetEvents, ICustomTypeDescriptor.GetEventsFromRegisteredType
             Return TypeDescriptor.GetEvents(_jsonObject, True)
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function GetProperties1(attributes As Attribute()) As PropertyDescriptorCollection Implements ICustomTypeDescriptor.GetProperties
             Return _propertyCollection
         End Function
 
-        Private Function GetProperties2() As PropertyDescriptorCollection Implements ICustomTypeDescriptor.GetProperties
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
+        Private Function GetProperties2() As PropertyDescriptorCollection Implements ICustomTypeDescriptor.GetProperties, ICustomTypeDescriptor.GetPropertiesFromRegisteredType
             Return CType(Me, ICustomTypeDescriptor).GetProperties(Nothing)
         End Function
 
+        <MethodImpl(MethodImplOptions.AggressiveInlining)>
         Private Function GetPropertyOwner(pd As PropertyDescriptor) As Object Implements ICustomTypeDescriptor.GetPropertyOwner
             Return _jsonObject
         End Function
