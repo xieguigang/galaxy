d0b66ccaa48b95cb70bb717cba50b66563eb752f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-13 18:58:22 +0800

    build ribbon menu

---
+++ src/LicenseFramework/KeySigned/RibbonMarkup.h
@@ -0,0 +1,19 @@
+// *****************************************************************************
+// * This is an automatically generated header file for UI Element definition  *
+// * resource symbols and values. Please do not modify manually.               *
+// *****************************************************************************
+
+#pragma once
+
+#define cmdButtonKeyGenerator 2 
+#define cmdButtonKeyGenerator_LabelTitle_RESID 60001
+#define cmdButtonKeyGenerator_SmallImages_192__RESID 60002
+#define cmdButtonKeyGenerator_LargeImages_192__RESID 60003
+#define cmdButtonLicenseGenerator 3 
+#define cmdButtonLicenseGenerator_LabelTitle_RESID 60004
+#define cmdButtonLicenseGenerator_SmallImages_192__RESID 60005
+#define cmdButtonLicenseGenerator_LargeImages_192__RESID 60006
+#define cmdPanelMain 4 
+#define cmdGroupMain 5 
+#define cmdGroupMain_LabelTitle_RESID 60007
+#define InternalCmd2_LabelTitle_RESID 60008
