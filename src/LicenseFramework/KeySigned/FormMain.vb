056398b5a5356a70276aa0884a3f7dc045f875e4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-13 18:47:13 +0800

    try to move to winform designer

--- src/LicenseFramework/KeySigned/FormMain.vb
+++ src/LicenseFramework/KeySigned/FormMain.vb
@@ -1,6 +1,4 @@
-﻿Imports KeySigned.LicenseFramework.Vendor
-
-Public Class FormMain
+﻿Public Class FormMain
 
     Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
         Call New KeyGeneratorForm().ShowDialog()
--- src/LicenseFramework/KeySigned/Pages/KeyGeneratorForm.vb
+++ src/LicenseFramework/KeySigned/Pages/KeyGeneratorForm.vb
@@ -9,76 +9,98 @@ Imports LicenseVendor.LicenseFramework.[Shared]
 Public Class KeyGeneratorForm
     Inherits Form
 
-    Private txtPrivateKey As New TextBox()
-    Private txtPublicKey As New TextBox()
+    Private WithEvents txtPrivateKey As TextBox
+    Private WithEvents txtPublicKey As TextBox
+
+    Dim WithEvents lblTitle As Label
+    WithEvents btnGenerate As Button
+    Dim WithEvents lblPrompt As Label
+    Dim WithEvents lblPrompt2 As Label
+    WithEvents btnSavePrivate As Button
+    WithEvents btnSavePublic As Button
 
     Public Sub New()
+        Call InitializeComponent()
+    End Sub
+
+    Private Sub InitializeComponent()
         Me.Text = "RSA密钥对生成工具"
         Me.Size = New Drawing.Size(700, 500)
         Me.StartPosition = FormStartPosition.CenterScreen
 
-        Dim lblTitle As New Label With {
-            .Text = "RSA 2048位密钥对生成工具",
-            .Font = New Drawing.Font("Microsoft YaHei", 14, Drawing.FontStyle.Bold),
-            .Location = New Drawing.Point(20, 15),
-            .AutoSize = True
-        }
-        Me.Controls.Add(lblTitle)
+        lblTitle = New Label
+        lblTitle.Text = "RSA 2048位密钥对生成工具"
+        lblTitle.Font = New Drawing.Font("Microsoft YaHei", 14, Drawing.FontStyle.Bold)
+        lblTitle.Location = New Drawing.Point(20, 15)
+        lblTitle.AutoSize = True
 
-        Dim btnGenerate As New Button With {
-            .Text = "生成密钥对",
-            .Size = New Drawing.Size(120, 35),
-            .Location = New Drawing.Point(20, 55)
-        }
-        AddHandler btnGenerate.Click, AddressOf BtnGenerate_Click
-        Me.Controls.Add(btnGenerate)
+
+
+        btnGenerate = New Button
+        btnGenerate.Text = "生成密钥对"
+        btnGenerate.Size = New Drawing.Size(120, 35)
+        btnGenerate.Location = New Drawing.Point(20, 55)
+
+
+
+
+        lblPrompt = New Label
+        lblPrompt.Text = "私钥 (Private Key) - 请妥善保管，切勿泄露:"
+        lblPrompt.Location = New Drawing.Point(20, 105)
+        lblPrompt.AutoSize = True
 
         ' 私钥
-        Me.Controls.Add(New Label With {
-            .Text = "私钥 (Private Key) - 请妥善保管，切勿泄露:",
-            .Location = New Drawing.Point(20, 105),
-            .AutoSize = True
-        })
+
+
+        txtPrivateKey = New TextBox
 
         txtPrivateKey.Multiline = True
         txtPrivateKey.Size = New Drawing.Size(650, 120)
         txtPrivateKey.Location = New Drawing.Point(20, 125)
         txtPrivateKey.ReadOnly = True
         txtPrivateKey.ScrollBars = ScrollBars.Vertical
-        Me.Controls.Add(txtPrivateKey)
 
-        Dim btnSavePrivate As New Button With {
-            .Text = "保存私钥",
-            .Size = New Drawing.Size(100, 30),
-            .Location = New Drawing.Point(20, 250)
-        }
-        AddHandler btnSavePrivate.Click, AddressOf BtnSavePrivate_Click
-        Me.Controls.Add(btnSavePrivate)
+
+        btnSavePrivate = New Button
+        btnSavePrivate.Text = "保存私钥"
+        btnSavePrivate.Size = New Drawing.Size(100, 30)
+        btnSavePrivate.Location = New Drawing.Point(20, 250)
+
+
+
+        lblPrompt2 = New Label
+        lblPrompt2.Text = "公钥 (Public Key) - 嵌入到客户端程序中:"
+        lblPrompt2.Location = New Drawing.Point(20, 290)
+        lblPrompt2.AutoSize = True
 
         ' 公钥
-        Me.Controls.Add(New Label With {
-            .Text = "公钥 (Public Key) - 嵌入到客户端程序中:",
-            .Location = New Drawing.Point(20, 290),
-            .AutoSize = True
-        })
+
+
+        txtPublicKey = New TextBox
 
         txtPublicKey.Multiline = True
         txtPublicKey.Size = New Drawing.Size(650, 120)
         txtPublicKey.Location = New Drawing.Point(20, 310)
         txtPublicKey.ReadOnly = True
         txtPublicKey.ScrollBars = ScrollBars.Vertical
-        Me.Controls.Add(txtPublicKey)
 
-        Dim btnSavePublic As New Button With {
-            .Text = "保存公钥",
-            .Size = New Drawing.Size(100, 30),
-            .Location = New Drawing.Point(20, 435)
-        }
-        AddHandler btnSavePublic.Click, AddressOf BtnSavePublic_Click
+
+        btnSavePublic = New Button
+        btnSavePublic.Text = "保存公钥"
+        btnSavePublic.Size = New Drawing.Size(100, 30)
+        btnSavePublic.Location = New Drawing.Point(20, 435)
+
+        Me.Controls.Add(lblTitle)
+        Me.Controls.Add(btnGenerate)
+        Me.Controls.Add(lblPrompt)
+        Me.Controls.Add(txtPrivateKey)
+        Me.Controls.Add(btnSavePrivate)
+        Me.Controls.Add(lblPrompt2)
+        Me.Controls.Add(txtPublicKey)
         Me.Controls.Add(btnSavePublic)
     End Sub
 
-    Private Sub BtnGenerate_Click(sender As Object, e As EventArgs)
+    Private Sub BtnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
         Try
             Me.Cursor = Cursors.WaitCursor
             Dim publicKeyXml As String = String.Empty
@@ -98,7 +120,7 @@ Public Class KeyGeneratorForm
         End Try
     End Sub
 
-    Private Sub BtnSavePrivate_Click(sender As Object, e As EventArgs)
+    Private Sub BtnSavePrivate_Click(sender As Object, e As EventArgs) Handles btnSavePrivate.Click
         If String.IsNullOrEmpty(txtPrivateKey.Text) Then Return
         Using dlg As New SaveFileDialog With {
             .Filter = "XML文件|*.xml",
@@ -112,7 +134,7 @@ Public Class KeyGeneratorForm
         End Using
     End Sub
 
-    Private Sub BtnSavePublic_Click(sender As Object, e As EventArgs)
+    Private Sub BtnSavePublic_Click(sender As Object, e As EventArgs) Handles btnSavePublic.Click
         If String.IsNullOrEmpty(txtPublicKey.Text) Then Return
         Using dlg As New SaveFileDialog With {
             .Filter = "XML文件|*.xml",
