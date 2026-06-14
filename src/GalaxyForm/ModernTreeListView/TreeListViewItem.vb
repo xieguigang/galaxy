746995242610994c9e0d1ff1fad5de2b8d5c637b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-15 03:35:21 +0800

    create abstract tree listview item model

---
+++ src/GalaxyForm/ModernTreeListView/TreeListViewItem.vb
@@ -0,0 +1,19 @@
+﻿Namespace ModernTreeListView
+
+    Public Class TreeListViewItem(Of T As TreeListViewItem(Of T))
+
+        Public Property Text As String
+        Public Property Tag As Object
+        Public Property Items As New List(Of T)
+        Public Property Parent As TreeListViewItem(Of T)
+        Public Property ImageIndex As Integer
+        Public Property ToolTipText As String
+
+        Public ReadOnly Property ChildrenCount As Integer
+            Get
+                Return Items.Count
+            End Get
+        End Property
+
+    End Class
+End Namespace
