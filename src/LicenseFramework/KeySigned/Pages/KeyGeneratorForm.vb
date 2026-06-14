1f7d9a1ad65143c7f051a352d1a17f1fc14c57c8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-14 15:41:32 +0800

    make code style improvements

--- src/LicenseFramework/KeySigned/Pages/KeyGeneratorForm.vb
+++ src/LicenseFramework/KeySigned/Pages/KeyGeneratorForm.vb
@@ -17,6 +17,11 @@ Public Class KeyGeneratorForm : Inherits DocumentWindow
 
     WithEvents btnSavePrivate As Button
     WithEvents btnSavePublic As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Private WithEvents TextBox2 As TextBox
     WithEvents btnGenerate As Button
 
     Public Sub New()
@@ -32,6 +37,11 @@ Public Class KeyGeneratorForm : Inherits DocumentWindow
         lblPrompt2 = New Label()
         txtPublicKey = New TextBox()
         btnSavePublic = New Button()
+        Button1 = New Button()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
         SuspendLayout()
         ' 
         ' lblTitle
@@ -106,9 +116,61 @@ Public Class KeyGeneratorForm : Inherits DocumentWindow
         btnSavePublic.TabIndex = 7
         btnSavePublic.Text = "保存公钥"
         ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(20, 630)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(97, 30)
+        Button1.TabIndex = 8
+        Button1.Text = "混淆"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.ForeColor = Color.Red
+        Label1.Location = New Point(20, 475)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(280, 15)
+        Label1.TabIndex = 9
+        Label1.Text = "重新生成密钥对之前，请先备份旧的密钥对文件"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(236, 630)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(52, 23)
+        TextBox1.TabIndex = 10
+        TextBox1.Text = "167"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(132, 633)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(98, 15)
+        Label2.TabIndex = 11
+        Label2.Text = "异或混淆种子："
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(20, 504)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.ReadOnly = True
+        TextBox2.ScrollBars = ScrollBars.Vertical
+        TextBox2.Size = New Size(650, 120)
+        TextBox2.TabIndex = 12
+        ' 
         ' KeyGeneratorForm
         ' 
-        ClientSize = New Size(709, 487)
+        AutoScaleDimensions = New SizeF(96.0F, 96.0F)
+        ClientSize = New Size(691, 772)
+        Controls.Add(TextBox1)
+        Controls.Add(TextBox2)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
         Controls.Add(lblTitle)
         Controls.Add(btnGenerate)
         Controls.Add(lblPrompt)
@@ -117,8 +179,12 @@ Public Class KeyGeneratorForm : Inherits DocumentWindow
         Controls.Add(lblPrompt2)
         Controls.Add(txtPublicKey)
         Controls.Add(btnSavePublic)
+        DockAreas = Microsoft.VisualStudio.WinForms.Docking.DockAreas.Float Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockLeft Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockRight Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockTop Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockBottom Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.Document
+        DoubleBuffered = True
         Name = "KeyGeneratorForm"
+        ShowHint = Microsoft.VisualStudio.WinForms.Docking.DockState.Unknown
         StartPosition = FormStartPosition.CenterScreen
+        TabPageContextMenuStrip = DockContextMenuStrip1
         Text = "RSA密钥对生成工具"
         ResumeLayout(False)
         PerformLayout()
@@ -193,6 +259,50 @@ Public Class KeyGeneratorForm : Inherits DocumentWindow
             txtPrivateKey.Text = .privateKey
             txtPublicKey.Text = .publicKey
         End With
+    End Sub
+
+    ''' <summary>
+    ''' GenerateObfuscatedKey()
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' 1. 你的原始公钥
+        Dim originalKey As String = txtPublicKey.Text
+        ' 2. 定义一个异或密钥 (你可以随意修改这个数值，比如 &HA7, &H5C 等，只要解密时用一样的就行)
+        Dim xorKey As Byte = CByte(TextBox1.Text) ' 167
+        ' 3. 将字符串转换为字节数组
+        Dim keyBytes As Byte() = Encoding.UTF8.GetBytes(originalKey)
+
+        ' 4. 进行 XOR 加密
+        For i As Integer = 0 To keyBytes.Length - 1
+            keyBytes(i) = keyBytes(i) Xor xorKey
+        Next
+
+        ' 5. 转换为 Base64 字符串 (因为XOR后可能产生不可见字符，Base64确保都是可见字符方便硬编码)
+        Dim base64Encrypted As String = Convert.ToBase64String(keyBytes)
+
+        ' 6. 拆分成多个片段 (这里拆成4段，你可以自由决定段数和每段长度)
+        Dim segLen As Integer = base64Encrypted.Length \ 4
+        Dim parts(3) As String
+        parts(0) = base64Encrypted.Substring(0, segLen)
+        parts(1) = base64Encrypted.Substring(segLen, segLen)
+        parts(2) = base64Encrypted.Substring(segLen * 2, segLen)
+        parts(3) = base64Encrypted.Substring(segLen * 3)
+
+        Dim sb As New StringBuilder
+
+        ' 7. 输出结果，你将把输出的结果复制到你的正式项目中
+        sb.AppendLine("' 请将以下数组和密钥复制到你的正式项目代码中：")
+        sb.AppendLine($"Dim xorKey As Byte = &H{xorKey:X2}")
+        sb.AppendLine($"Dim keyParts() As String = {{ 
+   ""{parts(0)}"", 
+   ""{parts(1)}"", 
+   ""{parts(2)}"", 
+   ""{parts(3)}"" 
+}}")
+
+        TextBox2.Text = sb.ToString
     End Sub
 End Class
 
