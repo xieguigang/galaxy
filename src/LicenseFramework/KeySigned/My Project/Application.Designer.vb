aba9e2fc8e47f20c1e38298df8fbbeddf0d335e6
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-16 08:27:03 +0800

    app file renames

--- src/LicenseFramework/KeySigned/My Project/Application.Designer.vb
+++ src/LicenseFramework/KeySigned/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.KeySigned.FormMain
+            Me.MainForm = Global.KeySignedTool.FormMain
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
