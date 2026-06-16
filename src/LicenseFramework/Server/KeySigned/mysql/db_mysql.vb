a9674e2e99c0623ea98257da061f8f3348178726
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-16 09:54:12 +0800

    add mysql database source files

---
+++ src/LicenseFramework/Server/KeySigned/mysql/db_mysql.vb
@@ -0,0 +1,18 @@
+﻿Imports Oracle.LinuxCompatibility.MySQL
+Imports Oracle.LinuxCompatibility.MySQL.MySqlBuilder
+Imports Oracle.LinuxCompatibility.MySQL.Uri
+
+Namespace license_svrModel
+
+Public MustInherit Class db_mysql : Inherits IDatabase
+Protected ReadOnly m_license As TableModel(Of license)
+Protected ReadOnly m_user As TableModel(Of user)
+Protected Sub New(mysqli As ConnectionUri)
+Call MyBase.New(mysqli)
+
+Me.m_license = model(Of license)()
+Me.m_user = model(Of user)()
+End Sub
+End Class
+
+End Namespace
