f08c4ef6669470dda4ca9d1eded66edc02c53da3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-06-16 08:34:39 +0800

    add parameter description

--- src/LicenseFramework/Server/KeySigned/Program.vb
+++ src/LicenseFramework/Server/KeySigned/Program.vb
@@ -10,7 +10,10 @@ Module Program
 
     <ExportAPI("/generate")>
     <Description("generates the software license file")>
-    <Usage("/generate --data <request.txt> --mysqli <mysqli.txt>")>
+    <Usage("/generate --data <request.txt> --mysqli <mysqli.txt> [--outfile <license.lic>]")>
+    <Argument("--data", , CLITypes.File, PipelineTypes.std_in, Description:="the license request file")>
+    <Argument("--mysqli",, CLITypes.File, Description:="the text file that contains the mysql connection uri string.")>
+    <Argument("--outfile",, CLITypes.File, PipelineTypes.std_out, Description:="the generated license file.")>
     Public Function GenerateLicense(data As String, mysqli As String, args As CommandLine) As Integer
 
     End Function
